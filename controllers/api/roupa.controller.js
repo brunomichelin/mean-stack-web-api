95890cce0f50ae4ada858676bfe9b440fa9cb844
bhmsilva <bhmsilva@bbmapfre.com.br>
2019-09-24 08:00:04 -0300

    Subida

--- controllers/api/roupa.controller.js
+++ controllers/api/roupa.controller.js
@@ -3,14 +3,18 @@ var router = express.Router();
 var roupaService = require('services/roupa.service');
 
 // routes
-router.post('/create', registerRoupa);
-router.get('/getAll', getRoupas);
+router.post('/addproduct', addProduct);
+router.get('/search/:_name', searchName);
+router.get('/:_id', getProduct);
+router.get('', getAll);
+router.put('/:_id', updateProduct);
+router.delete('/:_id', deleteProduct);
+
 
 module.exports = router;
 
-function registerRoupa(req, res) {
-    
-    roupaService.Create(req.body)
+function addProduct(req, res) {
+    roupaService.create(req.body)
         .then(function () {
             res.sendStatus(200);
         })
@@ -19,11 +23,59 @@ function registerRoupa(req, res) {
         });
 }
 
-function getRoupas(req, res) {
-    
-    roupaService.GetAll().then(function (roupas) {
-            if (roupas) {
-                res.send(roupas);
+function getProduct(req, res) {
+    roupaService.getById(req.params._id)
+        .then(function (productObject) {
+            if (productObject) {
+                res.send(productObject);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getAll(req, res) {
+    roupaService.getAll()
+        .then(function (productList) {
+            if (productList) {
+                res.send(productList);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function updateProduct(req, res) {
+    roupaService.update(req.params._id, req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function deleteProduct(req, res) {
+    roupaService.delete(req.params._id)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function searchName(req, res) {
+    roupaService.searchByName(req.params._name)
+        .then(function (productList) {
+            if (productList) {
+                res.send(productList);
             } else {
                 res.sendStatus(404);
             }
--- services/roupa.service.js
+++ services/roupa.service.js
@@ -9,37 +9,165 @@ db.bind('roupas');
 
 var service = {};
 
-service.GetAll = GetAll;
-service.Create = Create;
+service.getById = getById;
+service.searchByName = searchByName;
+service.getAll = getAll;
+service.create = create;
+service.update = update;
+service.delete = _delete;
 
 module.exports = service;
 
-function GetAll() {
-
+function getById(_id) {
     var deferred = Q.defer();
-    db.roupas.find().toArray(function(err, items) {
-        if (items) {  
-            deferred.resolve(items);
-        } else {            
+
+    db.roupas.findById(_id, function (err, productObject) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (productObject) {
+            // return product
+            deferred.resolve(_.omit(productObject, 'hash'));
+        } else {
+            // user not found
             deferred.resolve();
         }
-        console.log(deferred.promise)
     });
+
     return deferred.promise;
-    
 }
 
-function Create(roupa) {
+function create(productObject) {
+    var deferred = Q.defer();
+    
+    // validation
+    db.roupas.findOne(
+        { code: productObject.code },
+        function (err, productObject) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            if (productObject) {
+                // product id already exists
+                deferred.reject('Product Code "' + productObject.code + '" is already used');
+            } else {
+                createProduct();
+            }
+        });
+
+    function createProduct() {
+        // set user object to userParam without the cleartext password
+        var product = productObject;
+
+        db.roupas.insert(
+            product,
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function update(_id, productObject) {
     var deferred = Q.defer();
 
-    db.roupas.insert(
-        roupa,
-        function (err, doc) {
+    // validation
+    db.roupas.findById(_id, function (err, product) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (product.code !== productObject.code) {
+            // product id has changed so check if the new code is already taken
+            db.roupas.findOne(
+                { code: productObject.code },
+                function (err, productObject) {
+                    if (err) deferred.reject(err.name + ': ' + err.message);
+
+                    if (productObject) {
+                        // product already exists
+                        deferred.reject('Product Code "' + req.body.code + '" is already taken')
+                    } else {
+                        updateProduct();
+                    }
+                });
+        } else {
+            updateProduct();
+        }
+    });
+
+    function updateProduct() {
+        // fields to update
+        var set = {
+            date: productObject.date,
+            type: productObject.type,
+            brand: productObject.brand,
+            charac: productObject.charac,
+            size: productObject.size,
+            color: productObject.color,
+            labelprice: productObject.labelprice,
+            pricepaid: productObject.pricepaid,
+            suggestedprice: productObject.suggestedprice
+        };
+
+        db.roupas.update(
+            { _id: mongo.helper.toObjectID(_id) },
+            { $set: set },
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function _delete(_id) {
+    var deferred = Q.defer();
+
+    db.roupas.remove(
+        { _id: mongo.helper.toObjectID(_id) },
+        function (err) {
             if (err) deferred.reject(err.name + ': ' + err.message);
 
             deferred.resolve();
         });
-    
+
+    return deferred.promise;
+}
+
+function searchByName(name) {
+    var deferred = Q.defer();
+
+    db.roupas.findOne(
+        { name: name },
+        function (err, productList) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            if (productList) {
+                deferred.resolve(_.omit(productObject, 'hash'));
+            } else {
+                deferred.reject('The search returned no results');
+            }
+        });
+
+    return deferred.promise;
+}
+
+function getAll() {
+    var deferred = Q.defer();
+
+    db.roupas.find({}).toArray(function (err, productList) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (productList) {
+            // return product
+            deferred.resolve(_.omit(productList, 'hash'));
+        } else {
+            // user not found
+            deferred.resolve();
+        }
+    });
 
     return deferred.promise;
 }
